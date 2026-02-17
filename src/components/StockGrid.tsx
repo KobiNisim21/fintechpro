8c7d556411ed56f6792d99aab7a11be92ef4ddaf
Kobin <kobinisim21@gmail.com>
2026-02-17 20:10:38 +0200

    fix: revert desktop layout to grid-cols-3

--- src/components/StockGrid.tsx
+++ src/components/StockGrid.tsx
@@ -6,20 +6,10 @@ export function StockGrid() {
   const { positions } = usePortfolio();
 
   return (
-    <>
-      {/* Mobile/Tablet Grid (visible below 1280px) */}
-      <div className="grid grid-cols-1 gap-4 pb-20 min-[1280px]:hidden">
-        {positions.map((stock) => (
-          <StockCard key={stock._id} stock={stock} className="w-full" />
-        ))}
-      </div>
-
-      {/* Desktop Flex (visible above 1280px) */}
-      <div className="hidden min-[1280px]:flex flex-wrap gap-4 justify-start pb-20">
-        {positions.map((stock) => (
-          <StockCard key={stock._id} stock={stock} className="w-auto" />
-        ))}
-      </div>
-    </>
+    <div className="grid grid-cols-1 min-[1280px]:grid-cols-3 gap-4 pb-20">
+      {positions.map((stock) => (
+        <StockCard key={stock._id} stock={stock} className="w-full" />
+      ))}
+    </div>
   );
 }
